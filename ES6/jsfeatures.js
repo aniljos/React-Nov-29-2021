b51cc157594dcaa9fa355af36d6c007dde9e7abf
Anil Joseph <anil.jos@gmail.com>
2021-12-01 10:30:31 +0530

    Added

---
+++ ES6/jsfeatures.js
@@ -0,0 +1,9 @@
+var numbers = [1,2,3,4,5,6];
+
+
+var squares = numbers.map((item, index) => {
+    return item * item;
+});
+
+console.log(numbers);
+console.log(squares);
--- the-awesome-app/src/components/ListProducts.js
+++ the-awesome-app/src/components/ListProducts.js
@@ -4,6 +4,9 @@ import axios  from 'axios';
 
 class ListProducts extends Component{
 
+    state = {
+        products: []
+    }
 
     async componentDidMount(){
 
@@ -13,6 +16,8 @@ class ListProducts extends Component{
             
             const response = await axios.get(url);
             console.log("response", response);
+            this.setState({products: response.data});
+
 
         } catch (error) {
 
@@ -26,6 +31,18 @@ class ListProducts extends Component{
         return (
             <div>
                 <h4>Products</h4>
+                <div>
+                    {this.state.products.map((item, index) => {
+                        return (
+                            <div>
+                                <p>Id: {item.id}</p>
+                                <p>Name: {item.name}</p>
+                                <p>Description: {item.description}</p>
+                                <p>Price: {item.price}</p>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         )
     }
